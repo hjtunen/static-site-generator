07895453426937617e0a8058d45c3589aa641789
hjtunen <anonfinn92@gmail.com>
2026-04-09 14:38:53 +0300

    leaf node and tests

---
+++ src/leafnode.py
@@ -0,0 +1,20 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+        
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Leaf nodes must have a value")
+        if self.tag is None:
+            return self.value
+        if self.tag == "a":
+            return f"<a href=\"{self.props["href"]}\">{self.value}</a>"
+        if self.tag == "img":
+            return f"<img src=\"{self.props["src"]}\" alt=\"{self.value}\" />"
+        
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"tag = {self.tag}, value = {self.value}, props = {self.props}"
---
+++ src/test_leafnode.py
@@ -0,0 +1,40 @@
+import unittest
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_no_value(self):
+
+        node = LeafNode("b", None)
+        
+        with self.assertRaises(ValueError):
+            node.to_html()
+        
+    def test_leaf_to_html_no_tag(self):
+
+        node = LeafNode(None, "Value")
+        
+        self.assertEqual(node.to_html(), "Value")
+        
+    def test_leaf_repr(self):
+
+        node = LeafNode(tag="h1", value="Header")
+        answer = node.__repr__()
+
+        self.assertTrue("h1" in answer and "Header" in answer and "children" not in answer)
+        
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "google", props={"href": "google.com"})
+        self.assertEqual(node.to_html(), '<a href="google.com">google</a>')
+        
+    def test_leaf_to_html_img(self):
+        node = LeafNode("img", "Cat pic", props={"src": "image.jpg"})
+        self.assertEqual(node.to_html(), '<img src="image.jpg" alt="Cat pic" />')
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
